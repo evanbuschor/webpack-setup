d97180653a7d0d458d9187e11009ee544841cf9f
Evan Buschor <ebuschor@assurecare.com>
2022-07-27 13:07:40 -0400

    initial webpack setup with test code

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const Keyboard = {
+    keyLayout: "ortholinear ergodox",
+    isSplit: true,
+    keyCapName: "SA Vilebloom",
+    KeyCapProfile: "SA",
+    switchType: "NovelKeys Silk Red",
+    switchLube: "Krytox 205g",
+    caseColor: "white",
+    hasUnderGlow: true
+}
+
+console.log(Keyboard)
